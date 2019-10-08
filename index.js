c078d3a9df9f1b86c70dadd9a65d7ccff3e1a554
trillobite <jparnell0@protonmail.com>
2019-10-08 13:05:23 -0700

    gets cookies

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
 const request = require("request");
+// const Cookie = require("request-cookies").Cookie;
+// const cookieJar = require("request-cookies").CookieJar;
 const zmRoot = "http://192.168.100.57/zm/api";
 
 
 let now = new Date();
 
-console.log(now.getDate());
-console.log(now.getDay());
-console.log(now.getHours());
+// console.log(now.getDate());
+// console.log(now.getDay());
+// console.log(now.getHours());
 
 let setMode = async (mode, camera) => {
     try {
-        await request.post({url:`${zmRoot}/host/login.json`, form: {
-            user: "admin",
-            pass: "BnCQd9*B7i"
-        }}, (err,httpResponse,body) => {
-            console.log("loginResponse:", httpResponse.headers["set-cookie"]);
+        await request.post({
+            url: `${zmRoot}/host/login.json`, form: {
+                user: "admin",
+                pass: "BnCQd9*B7i"
+            }
+        }, async (err, httpResponse, body) => {
+            // let rawcookies = httpResponse.headers["set-cookie"];
+            // rawcookies.map((raw) => {
+            //     let cookie = new Cookie(raw);
+            //     cookieJar.apply(cookie);
+            // })
+
+            console.log("COOKIES:", httpResponse.headers["set-cookie"]);
+
+            await request.post({
+                url: `${zmRoot}/monitors/${camera}.json`, form: {
+                    "Monitor[Function]": `${mode}`
+                }
+            }, (err, httpResponse, body) => {
+                console.log("cameraResponse:", body);
+            });
+
         });
 
 
-        // await request.post({url:`${zmRoot}/monitors/${camera}.json`, form: {
-        //     "Monitor[Function]": `${mode}`
-        // }}, (err,httpResponse,body) => {
-        //     console.log("cameraResponse:", body);
-        // });
 
     } catch (e) {
         console.log("Error:", e);
